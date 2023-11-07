b0389d680de68ae521fe1f47efb7df694c8ab848
avkn4998 <avkn4998@colorado.edu>
2023-11-07 10:24:32 -0700

    k

--- in-class/week11.js
+++ in-class/week11.js
@@ -29,6 +29,18 @@ function random(number) {
 
 function draw (){
     ctx.clearRect(0,0, canvas.width, canvas.height);
+    for (let i =0; i<100; i++){
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(255,0,0,5)";
+    ctx.arc(
+        random(canvas.width),
+        random(canvas.height),
+        random(50),
+        0,
+        2 * Math.PI,
+    )
+    ctx.fill();
+}
 }
 
 btn.addEventListener("click",draw);
