c9f4d35f1c9e9ec02d43f32e092ec873d213c06f
avkn4998 <avkn4998@colorado.edu>
2023-11-16 22:38:36 -0500

    k

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -3,32 +3,37 @@ document.addEventListener('DOMContentLoaded', function () {
     const quoteText = document.getElementById('quote-text');
     const quoteAuthor = document.getElementById('quote-author');
     const getQuoteButton = document.getElementById('get-quote');
+    const shareTwitterButton = document.getElementById('share-twitter'); 
   
-    // Function to fetch a random Dad Joke
+   
     async function getDadJoke() {
-      try {
-        const response = await fetch(endpoint, {
-          headers: {
-            'Accept': 'application/json'
-          }
-        });
-        const data = await response.json();
-        return data.joke;
-      } catch (error) {
-        console.error('Error fetching Dad Joke:', error);
-      }
+        try {
+            const response = await fetch(endpoint, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            const data = await response.json();
+            return data.joke;
+        } catch (error) {
+            console.error('Error fetching Dad Joke:', error);
+        }
     }
-  
-    // Function to update the DOM with a new Dad Joke
+ 
     async function displayDadJoke() {
-      // Check if there is no existing Dad Joke text
-      if (!quoteText.textContent.trim()) {
         const joke = await getDadJoke();
         quoteText.textContent = joke;
-        quoteAuthor.textContent = ''; // Dad Jokes typically don't have an author
-      }
     }
   
-    // Event listener for the "Get Quote" button
     getQuoteButton.addEventListener('click', displayDadJoke);
-  });
+  
+    shareTwitterButton.addEventListener('click', function () {
+        const encodedQuote = encodeURIComponent(quoteText.textContent);
+  
+
+        const twitterShareURL = `https://twitter.com/intent/tweet?text=${encodedQuote}&hashtags=QuoteMachine`;
+  
+
+        window.open(twitterShareURL, '_blank');
+    });
+});
