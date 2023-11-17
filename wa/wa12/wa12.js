1ab7448b49f7e6bf121373564bf66dc31f517244
avkn4998 <avkn4998@colorado.edu>
2023-11-16 22:13:01 -0500

    k

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,31 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const endpoint = 'https://api.quotable.io/random';
+    const endpoint = 'https://icanhazdadjoke.com/';
     const quoteText = document.getElementById('quote-text');
     const quoteAuthor = document.getElementById('quote-author');
     const getQuoteButton = document.getElementById('get-quote');
   
-    // Function to fetch a random quote
-    async function getQuote() {
+    // Function to fetch a random Dad Joke
+    async function getDadJoke() {
       try {
-        const response = await fetch(endpoint);
+        const response = await fetch(endpoint, {
+          headers: {
+            'Accept': 'application/json'
+          }
+        });
         const data = await response.json();
-        return data;
+        return data.joke;
       } catch (error) {
-        console.error('Error fetching quote:', error);
+        console.error('Error fetching Dad Joke:', error);
       }
     }
   
-    // Function to update the DOM with a new quote
-    function displayQuote() {
-      getQuote().then(quote => {
-        quoteText.textContent = quote.content;
-        quoteAuthor.textContent = `- ${quote.author}`;
-      });
+    // Function to update the DOM with a new Dad Joke
+    async function displayDadJoke() {
+      // Check if there is no existing Dad Joke text
+      if (!quoteText.textContent.trim()) {
+        const joke = await getDadJoke();
+        quoteText.textContent = joke;
+        quoteAuthor.textContent = ''; // Dad Jokes typically don't have an author
+      }
     }
   
     // Event listener for the "Get Quote" button
-    getQuoteButton.addEventListener('click', displayQuote);
-  
-    // Display an initial quote when the page loads
-    displayQuote();
+    getQuoteButton.addEventListener('click', displayDadJoke);
   });
