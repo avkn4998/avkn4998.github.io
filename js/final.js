14448e76af7bafe727d98ec3aca424e738a21652
avkn4998 <avkn4998@colorado.edu>
2023-12-10 17:10:54 -0700

    k

--- js/final.js
+++ js/final.js
@@ -33,11 +33,11 @@ function createFloatingNumbers(digit, count) {
 
 function spawnFloatingNumbers(phoneNumber) {
     for (let i = 0; i < phoneNumber.length; i++) {
-        createFloatingNumbers(phoneNumber[i], 5); // Adjust the count as needed
+        createFloatingNumbers(phoneNumber[i], 5); 
     }
 }
 
-const phoneNumber = "1234567890"; // Your phone number
+const phoneNumber = "1234567890"; 
 spawnFloatingNumbers(phoneNumber);
 
 
@@ -49,7 +49,7 @@ function animateFloatingNumber(element) {
     function updatePosition() {
         const rect = element.getBoundingClientRect();
 
-        // Check if the element is going off the top or bottom of the screen
+      
         if (rect.top + speedY < 0) {
             element.style.top = window.innerHeight + 'px';
         } else if (rect.bottom + speedY > window.innerHeight) {
@@ -58,7 +58,7 @@ function animateFloatingNumber(element) {
             element.style.top = rect.top + speedY + 'px';
         }
 
-        // Check if the element is going off the left or right of the screen
+
         if (rect.left + speedX < 0) {
             element.style.left = window.innerWidth + 'px';
         } else if (rect.right + speedX > window.innerWidth) {
@@ -83,21 +83,11 @@ for (let i = 0; i < 10; i++) {
     createFloatingNumber(i);
 }
 
-function updateProgressBar() {
-    const phoneNumberInput = document.getElementById('phoneNumber');
-    const progressBar = document.getElementById('progressBar');
-    
-    // Set the width of the progress bar based on the percentage of completion
-    const percentage = (phoneNumberInput.textContent.length / 10) * 100; // Assuming a maximum length of 10
-    progressBar.style.width = percentage + '%';
-}
+
 
 function appendToPhoneNumber(number) {
     const phoneNumberInput = document.getElementById('phoneNumber');
     phoneNumberInput.textContent += number;
-
-    // Update the progress bar
-    updateProgressBar();
 }
 
 
